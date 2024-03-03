7da83c67c3c1c9a71d539eac24e0b0f1ed8ef05d
Genesis Ancheta <genex2anchetamax@gmail.com>
2024-03-03 14:05:16 +0800

    update name and link

--- client1/react/account-mandara/src/App.js
+++ client1/react/account-mandara/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Genesis Ancheta
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Genesis Anncheta
+          Want to know more?
         </a>
       </header>
     </div>
