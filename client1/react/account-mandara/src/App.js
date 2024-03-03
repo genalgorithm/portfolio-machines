5eebef650a17fa6bad9d895fee52424db767060e
Genesis Ancheta <genex2anchetamax@gmail.com>
2024-03-03 15:04:18 +0800

    update link

--- client1/react/account-mandara/src/App.js
+++ client1/react/account-mandara/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Want to know more?
+          Search up Genesis Ancheta?
         </a>
       </header>
     </div>
